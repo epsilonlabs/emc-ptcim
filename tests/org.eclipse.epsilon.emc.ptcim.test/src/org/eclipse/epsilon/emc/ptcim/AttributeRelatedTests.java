c74001959701aa4dbe1335cefc25fdb7500edf59
Athanasios Zolotas <zolotas4@gmail.com>
2016-11-12 12:28:32 +0000

    Test suite setup and dummy tests

---
+++ tests/org.eclipse.epsilon.emc.ptcim.test/src/org/eclipse/epsilon/emc/ptcim/AttributeRelatedTests.java
@@ -0,0 +1,14 @@
+package org.eclipse.epsilon.emc.ptcim;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AttributeRelatedTests {
+
+	@Test
+	public void testMutliplicityAndType() {		
+		assertEquals("x", "x");
+	}
+}
